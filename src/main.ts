c659279a2aa52e5a500270d0f3406176d15246c8
bluesky335 <chinabluesky335@gmail.com>
2024-04-21 00:38:46 +0800

    add css variable to svg's "fill" and "stroke" attribute. watching svg folder's add new file event and reload vite server

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,123 @@
 import { ModuleNode, Plugin } from "vite";
+import SvgSprite from "svg-sprite";
+import fs from "fs";
+import path from "path";
+import File from "vinyl";
+import { optimize, Config } from "svgo";
 
 export interface SvgXOptions {
   path: string[];
+  svgoConfig?: Config;
 }
 
 export function svgX(options: SvgXOptions): Plugin {
   return {
     name: "svg-x",
-    transformIndexHtml(html) {
-      console.log("transformIndexHtml");
-      return html.replace(/<head>/,'<head> <meta></meta>')
+    configureServer(server) {
+      server.watcher.add(options.path);
+      server.watcher.on("add", (path) => {
+        if (path.endsWith(".svg")) {
+          server.hot.send({ type: "full-reload" });
+        }
+      });
+    },
+    async transformIndexHtml(html) {
+      let sprite = new SvgSprite({
+        dest: "dist",
+        mode: {
+          symbol: true,
+        },
+        svg: {
+          xmlDeclaration: false,
+          doctypeDeclaration: false,
+          rootAttributes: {
+            style: "position:absolute;width:0px;height:0px;overflow:hidden;",
+          },
+        },
+      });
+      options.path.forEach((svgDirPath) => {
+        // 遍历子文件夹，查找其中的svg
+        let files = fs.readdirSync(svgDirPath);
+        while (files.length > 0) {
+          const file = files.shift()!;
+          let fullFilePath = path.join(svgDirPath, file);
+          // 判断是不是目录
+          const isDirectory = fs.lstatSync(fullFilePath).isDirectory();
+          if (isDirectory) {
+            let subPaths = fs
+              .readdirSync(fullFilePath)
+              .map((item) => path.join(file, item));
+            files.push(...subPaths);
+            continue;
+          }
+          if (!fullFilePath.endsWith(".svg")) {
+            continue;
+          }
+          let svgString = fs.readFileSync(fullFilePath, "utf-8");
+          let svgoConfig: Config = {
+            path: fullFilePath,
+            multipass: true,
+            plugins: [
+              {
+                name: "convertStyleToAttrs",
+                params: {
+                  keepImportant: false,
+                },
+              },
+              {
+                name: "fill-colors",
+                fn: () => {
+                  return {
+                    element: {
+                      enter: (node) => {
+                        if (
+                          node.attributes.fill &&
+                          !node.attributes.fill.startsWith("var(")
+                        ) {
+                          node.attributes.fill = `var(--svg-fill,${node.attributes.fill})`;
+                        }
+                        if (!node.attributes.fill) {
+                          node.attributes.fill = `var(--svg-fill)`;
+                        }
+                        if (
+                          node.attributes.stroke &&
+                          !node.attributes.stroke.startsWith("var(")
+                        ) {
+                          node.attributes.stroke = `var(--svg-stroke,${node.attributes.stroke})`;
+                        }
+                        if (!node.attributes.stroke) {
+                          node.attributes.stroke = `var(--svg-stroke)`;
+                        }
+                      },
+                    },
+                  };
+                },
+              },
+              // {
+              //   name: "removeAttrs",
+              //   params: {
+              //     attrs: "(fill|stroke)",
+              //     preserveCurrentColor: true,
+              //   },
+              // },
+            ],
+          };
+          if (options.svgoConfig) {
+            svgoConfig = Object.assign(svgoConfig, options.svgoConfig);
+          }
+          const result = optimize(svgString, svgoConfig);
+          const optimizedSvgString = result.data;
+          sprite.add(path.join(svgDirPath, file), file, optimizedSvgString);
+        }
+      });
+      let spriteStr = "";
+      sprite.compile((error, result, data) => {
+        // 读取 File 对象的内容转为字符串
+        spriteStr = (result.symbol.sprite as File).contents?.toString() ?? "";
+      });
+      return html.replace("<body>", `<body>${spriteStr}`);
     },
     handleHotUpdate({ file, server, modules, timestamp }) {
-      console.log("handleHotUpdate", file, modules.length);
       if (
         (file.endsWith(".svg"),
         options.path.findIndex((i) => file.startsWith(i)) > -1)
@@ -22,7 +127,6 @@ export function svgX(options: SvgXOptions): Plugin {
       // 手动使模块失效
       const invalidatedModules = new Set<ModuleNode>();
       for (const mod of modules) {
-        console.log(mod.id);
         server.moduleGraph.invalidateModule(
           mod,
           invalidatedModules,
@@ -30,7 +134,7 @@ export function svgX(options: SvgXOptions): Plugin {
           true
         );
       }
-      return [];
+      return modules;
     },
   };
 }
